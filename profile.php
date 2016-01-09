ffe00082404c5e8ba629201786432cf64667d98e
Stein Carsten Sinke <sinke.carsten95@gmail.com>
2016-01-09 22:43:11 +0100

    profile update

--- profile.php
+++ profile.php
@@ -8,7 +8,21 @@ $json = file_get_contents('http://86.107.110.214/portal/students/profile/'.$_COO
     $obj = json_decode($json);
     if (isset($obj->student)){
       echo '<h1>'.$obj->student->name.'</h1>';
-
+      echo '<h1>'.$obj->student->studentnumber.'</h1>';
+      echo '<h1>'.$obj->student->class.'</h1>';
+      echo '<h1>'.$obj->student->birthdate.'</h1>';
+      echo '<h1>'.$obj->student->phonenumbers->home.'</h1>';
+      echo '<h1>'.$obj->student->phonenumbers->mobile.'</h1>';
+      echo '<h1>'.$obj->student->adress->street.'</h1>';
+      echo '<h1>'.$obj->student->adress->zipcode.'</h1>';
+      echo '<h1>'.$obj->student->adress->place.'</h1>';
+      echo '<h1>'.$obj->student->mentor->name.'</h1>';
+      echo '<h1>'.$obj->student->mentor->abbreviation.'</h1>';
+      echo '<h1>'.$obj->student->mentor->email.'</h1>';
+      echo '<h1>'.$obj->student->Profile->profile.'</h1>';
+      echo '<h1>'.$obj->student->Profile->code.'</h1>';
+      echo '<h1>'.$obj->student->Profile->abbreviation.'</h1>';
+      echo '<h1>'.$obj->student->Profile->year.'</h1>';
     } else {
       $error = "Ongeldige gebruikersnaam of wachtwoord!";
     }
