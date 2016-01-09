c7da172b50712dd59ed750caee88eb2f73ab5419
chrisje <chriswitteveen2@hotmail.com>
2016-01-09 22:48:03 +0100

    error fix

--- cijfers.php
+++ cijfers.php
@@ -7,8 +7,9 @@ include_once("header.php");
 $json = file_get_contents('http://86.107.110.214/portal/students/grades/1/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
     $obj = json_decode($json);
     if (isset($obj->student)){
-      foreach ($obj->classes as $class)
-      echo '<h1>'.$class->text.'</h1>';
+      foreach ($obj->classes as $class) {
+        echo '<h1>'.$class->text.'</h1>'; 
+      }
 
     } else {
       $error = "Ongeldige gebruikersnaam of wachtwoord!";
