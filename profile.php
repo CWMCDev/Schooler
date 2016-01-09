7f37a2a943ea4dac27929dd79634199f978e3660
Stein Carsten Sinke <sinke.carsten95@gmail.com>
2016-01-09 23:27:27 +0100

    wrong obj fixed

--- profile.php
+++ profile.php
@@ -11,18 +11,18 @@ $json = file_get_contents('http://86.107.110.214/portal/students/profile/'.$_COO
       echo '<h1>'.$obj->student->studentnumber.'</h1>';
       echo '<h1>'.$obj->student->class.'</h1>';
       echo '<h1>'.$obj->student->birthdate.'</h1>';
-      echo '<h1>'.$obj->student->adress->street.'</h1>';
       echo '<h1>'.$obj->student->phonenumbers->home.'</h1>';
       echo '<h1>'.$obj->student->phonenumbers->mobile.'</h1>';
-      echo '<h1>'.$obj->student->adress->zipcode.'</h1>';
-      echo '<h1>'.$obj->student->adress->place.'</h1>';
-      echo '<h1>'.$obj->student->mentor->name.'</h1>';
-      echo '<h1>'.$obj->student->mentor->abbreviation.'</h1>';
-      echo '<h1>'.$obj->student->mentor->email.'</h1>';
-      echo '<h1>'.$obj->student->Profile->profile.'</h1>';
-      echo '<h1>'.$obj->student->Profile->code.'</h1>';
-      echo '<h1>'.$obj->student->Profile->abbreviation.'</h1>';
-      echo '<h1>'.$obj->student->Profile->year.'</h1>';
+      echo '<h1>'.$obj->adress->street.'</h1>';
+      echo '<h1>'.$obj->adress->zipcode.'</h1>';
+      echo '<h1>'.$obj->adress->place.'</h1>';
+      echo '<h1>'.$obj->mentor->name.'</h1>';
+      echo '<h1>'.$obj->mentor->abbreviation.'</h1>';
+      echo '<h1>'.$obj->mentor->email.'</h1>';
+      echo '<h1>'.$obj->Profile->profile.'</h1>';
+      echo '<h1>'.$obj->Profile->code.'</h1>';
+      echo '<h1>'.$obj->Profile->abbreviation.'</h1>';
+      echo '<h1>'.$obj->Profile->year.'</h1>';
     } else {
       $error = "Ongeldige gebruikersnaam of wachtwoord!";
     }
