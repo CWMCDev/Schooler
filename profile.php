5eed8ebbfa2fcd6790dc19f8b7f1a3c7b0117239
Milo Cesar <milocesar1@gmail.com>
2016-01-09 22:17:20 +0100

    String

--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@ include_once("header.php");
 $json = file_get_contents('http://86.107.110.214/portal/students/profile/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
     $obj = json_decode($json);
     if (isset($obj->student)){
-      echo '<h1>$obj->student->name</h1>';
+      echo '<h1>'.$obj->student->name.'</h1>';
 
     } else {
       $error = "Ongeldige gebruikersnaam of wachtwoord!";
