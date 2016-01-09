ca323bf9cf7d2069225d5d31cd9f2fd9fc6dfafe
Stein Carsten Sinke <sinke.carsten95@gmail.com>
2016-01-09 23:42:25 +0100

    classlist.php

---
+++ classlist.php
@@ -0,0 +1,20 @@
+<?php
+$title = "Profile";
+include_once("header.php");
+?>
+
+<?php
+$json = file_get_contents('http://86.107.110.214/portal/students/classlist/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
+    $obj = json_decode($json);
+    if (isset($obj->classList)){
+  			Echo '<p>'.$obj->classList->name.'</p>';
+  			Echo '<p>'.$obj->classlist->id.'</p>';
+    } else {
+      $error = "Ongeldige gebruikersnaam of wachtwoord!";
+    }
+?>
+
+
+<?php
+include_once("footer.php");
+?>
