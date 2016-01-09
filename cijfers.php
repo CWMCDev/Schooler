fc70ddebe7d3a689e4b2f9de5516a4bd9af8ee0d
chrisje <chriswitteveen2@hotmail.com>
2016-01-09 23:23:15 +0100

    You will now see your own grades

--- cijfers.php
+++ cijfers.php
@@ -4,7 +4,7 @@ include_once("header.php");
 ?>
 
 <?php
-$json = file_get_contents('http://86.107.110.214/portal/students/grades/1/cc111748/RnlQuFggpslULvtDz0iQ4MvvcdGz1R8NeSWliADpNyDSfF7SXlfkw1kwUvIfYSZBOnkldAfUbKfWPIYGvKnD5lJhLGNEd5jYE4KXaE1QCBuMOLfwy1nFiL25SID8dpComAfhxR5TPww21K42NKa1q258vLMSg5yNSt0fewsKOOWZdgG2kp3OIqT2OGb5dn5O7xQXHj3wlCltruxvLJx5zsywKiEh4kHYrVgWhQMuvIs0JePjglnbl8dDUM87k4b2');
+$json = file_get_contents('http://86.107.110.214/portal/students/grades/1/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
     $obj = json_decode($json);
     if (isset($obj->classes)){
       echo '<table style="width:100%">';
