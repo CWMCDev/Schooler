35f538cc0378b6d5e8786933d3c60f2e99118a60
chrisje <chriswitteveen2@hotmail.com>
2016-01-09 23:18:43 +0100

    grades now working

--- cijfers.php
+++ cijfers.php
@@ -4,11 +4,21 @@ include_once("header.php");
 ?>
 
 <?php
-$json = file_get_contents('http://86.107.110.214/portal/students/grades/1/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
+$json = file_get_contents('http://86.107.110.214/portal/students/grades/1/cc111748/RnlQuFggpslULvtDz0iQ4MvvcdGz1R8NeSWliADpNyDSfF7SXlfkw1kwUvIfYSZBOnkldAfUbKfWPIYGvKnD5lJhLGNEd5jYE4KXaE1QCBuMOLfwy1nFiL25SID8dpComAfhxR5TPww21K42NKa1q258vLMSg5yNSt0fewsKOOWZdgG2kp3OIqT2OGb5dn5O7xQXHj3wlCltruxvLJx5zsywKiEh4kHYrVgWhQMuvIs0JePjglnbl8dDUM87k4b2');
     $obj = json_decode($json);
-    if (isset($obj->student)){
+    if (isset($obj->classes)){
+      echo '<table style="width:100%">';
       foreach ($obj->classes as $class) {
-        echo '<h1>'.$class->text.'</h1>'; 
+        echo '<tr><td>'.$class->text.'</td>'; 
+        $gradesText = '';
+        foreach($class->grades as $grade) {
+          if ($gradesText == '') {
+            $gradesText .= $grade->Cijfer;
+          } else {
+            $gradesText .= " - ".$grade->Cijfer;
+          }
+        }
+        echo '<td>'.$gradesText.'</td></tr>';
       }
 
     } else {
