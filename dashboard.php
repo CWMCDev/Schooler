1df392bfeb1d79edcfabf1536a71563c1ae279aa
Stein Carsten Sinke <sinke.carsten95@gmail.com>
2016-01-10 21:53:01 +0100

    servers changed

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@ $title = "Dashboard";
 include_once("header.php");
 ?>
 <?php
-$json = file_get_contents('http://86.107.110.214/portal/students/profile/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
+$json = file_get_contents('http://api.8t2.eu/portal/students/profile/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
     $obj = json_decode($json);
     if (isset($obj->student->name)){
       echo $obj->student->name;
--- profile.php
+++ profile.php
@@ -4,7 +4,7 @@ include_once("header.php");
 ?>
 
 <?php
-$json = file_get_contents('http://86.107.110.214/portal/students/profile/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
+$json = file_get_contents('http://api.8t2.eu/portal/students/profile/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
     $obj = json_decode($json);
     if (isset($obj->student)){
       echo '<p>'.$obj->student->name.'</p>';
