bc3a60580a66603f2f3b347eda9f72d15745e1c0
Milo Cesar <milocesar1@gmail.com>
2016-01-11 21:33:51 +0100

    KLEURTJES

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $user = substr($_POST['username'],2);
     }
 
-    $json = file_get_contents('http://86.107.110.214/auth/register/'. $user .'/' . $_POST['password']);
+    $json = file_get_contents('http://api.8t2.eu/auth/register/'. $user .'/' . $_POST['password']);
     $obj = json_decode($json);
     if (isset($obj->token)){
       setcookie('id', "cc".$user, time() + (60 * 60 * 24 * 365), "/");
--- presention.php
+++ presention.php
@@ -12,16 +12,41 @@ $json = file_get_contents('http://86.107.110.214/portal/students/presention/'.$_
     if (isset($obj->presentie)){
       echo '<table style="width:100%">';
       foreach ($obj->presentie as $week) {
-        echo '<tr><td>'.$week->week.'</td>'; 
+        //echo '<tr><td>'.$week->week.'</td>'; 
         foreach($week->dagen as $day) {
-          echo "<td><article><table><tr><td>Uur</td><td>Status</td></tr>";
+          //echo "<td><article><table><tr><td>Uur</td><td>Status</td></tr>";
+          echo '<tr>';
           foreach($day as $hour) {
-            echo '<tr><td>'.$hour->hour."</td><td>".$hour->status.'</td></tr>';
+            $bg = '';
+            $msg = '';
+            switch ($hour->status) {
+              case 'aanw':
+                $bg = '4FD57F';
+                break;
+              
+              case 'geoorlafw';
+                $bg = '4FD57F';
+                $msg = 'geoorloofd afw';
+
+              case 'melding-only';
+                $bg = 'FF8C00';
+                $msg = 'Melding';
+
+              case 'afw';
+                $bg = 'FF0000';
+                $msg = 'Afwezig';
+              default:
+                # code...
+                break;
+            }
+            echo '<td bgcolor='.$bg.'>'.$hour->hour."</td><td>".$hour->status.'</td>';
           }
-          echo "</table></article></td>"; 
+          echo '</tr>';
+          //echo "</table></article></td>"; 
         }
-        echo '</tr>';
+        //echo '</tr>';
       }
+      echo '</table>';
     } else {
       $error = "Ongeldige gebruikersnaam of wachtwoord!";
     }
