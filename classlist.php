26e4167e2ee0bb0cda7bdeefc39396a0b6ed4f18
Willemijn Stam <willemijnstam@Willemijns-MacBook-Air.local>
2016-01-12 15:39:26 +0100

    classlist no table output

--- classlist.php
+++ classlist.php
@@ -7,12 +7,14 @@ include_once("header.php");
 $json = file_get_contents('http://api.8t2.eu/portal/students/classlist/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
     $obj = json_decode($json);
     if (isset($obj->classList)){
+    	echo '<div id="container">';
     	echo '<table class="table table-striped>';
         foreach ($obj->classList as $person) {
           echo '<tr><td>'.$person->name.'</td>';
           echo '<td>'.$person->id.'</td></tr>';
         }
         echo '</table>';
+        echo '</div>';
     } else {
       $error = "Ongeldige gebruikersnaam of wachtwoord!";
     }
