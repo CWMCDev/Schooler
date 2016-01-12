175c494ac015c3b3f6a96f39e01c35f6d4157716
Stein Carsten Sinke <sinke.carsten95@icloud.com>
2016-01-12 01:09:19 +0100

    bootstrap required metadata

--- header.php
+++ header.php
@@ -13,8 +13,10 @@ if(!isset($_COOKIE['token']) || !isset($_COOKIE['id'])) {
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Schooler - <?php echo($title); ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Latest jQuery -->
   <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
 
