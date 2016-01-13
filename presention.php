21d3731d010cc57701f2b9119658a84e30e1fc21
cwitteveen <chriswitteveen2@hotmail.com>
2016-01-13 13:30:27 +0100

    Trying to decrease loading time

--- presention.php
+++ presention.php
@@ -4,7 +4,8 @@ include_once("header.php");
 ?>
 
 <?php
-$json = file_get_contents('http://api.8t2.eu/portal/students/presention/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
+$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
+$json = file_get_contents('http://api.8t2.eu/portal/students/presention/'.$_COOKIE['id'].'/'.$_COOKIE['token'],false,$context);
     $obj = json_decode($json);
     
     echo '<br><br>';
