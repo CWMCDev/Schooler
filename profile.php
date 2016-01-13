6c0af622d786db63ea53a2bc473f89a733942483
cwitteveen <chriswitteveen2@hotmail.com>
2016-01-13 13:42:14 +0100

    Fixed error

--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@ include_once("header.php");
 $json = file_get_contents('http://api.8t2.eu/portal/students/profile/'.$_COOKIE['id'].'/'.$_COOKIE['token']);
     $obj = json_decode($json);
     if (isset($obj->student)){
-echo '<div id="student_short'>;
+echo '<div id="student_short">';
 echo '<table table-striped width="100px">';
 echo '<tr><td>'.$obj->student->name.'</td></tr>';
 echo '<tr><td>Leerlingnummer</td>';
