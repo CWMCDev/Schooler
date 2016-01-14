48859770390d99bda471bbb5feeb39a8537b99e1
Willemijn Stam <willemijnstam@Willemijns-MacBook-Air.local>
2016-01-14 11:05:39 +0100

    Trimmed var
    Milo still was not able to fix....

--- profile.php
+++ profile.php
@@ -19,7 +19,8 @@ $json = file_get_contents('http://api.8t2.eu/portal/students/profile/'.$_COOKIE[
         echo '<td>'.$obj->student->birthdate.'</td></tr>';
         echo '<tr><th>Telefoon privé</th>';
         echo '<td>'.$obj->student->phonenumbers->home.'</td></tr>';
-        if(!empty(trim($obj->student->phonenumbers->mobile)), " "){
+        $trimmedMobile = trim($obj->student->phonenumbers->mobile);
+        if(!empty($trimmedMobile), " "){
             echo '<tr><th>Telefoon mobile</th>';
             echo '<td>'.$obj->student->phonenumbers->mobile.'</td></tr>';
         }
