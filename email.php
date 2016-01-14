25fe465242eed86a259ca3f8f2dd358dba0f2aef
Milo Cesar <milocesar1@gmail.com>
2016-01-14 22:51:56 +0100

    Async load email

--- email.php
+++ email.php
@@ -1,8 +1,22 @@
 <?php
 $title = "Mail";
 include_once("header.php");
+?>
+<script type="text/javascript">
+	$(document).ready( function () {
+      var id = readCookie('id');
+      var token = readCookie('token');
 
+      var url = 'http://api.8t2.eu/mail/'+id+'/'+token;
 
-
+      $.ajax({
+        url: url,
+        dataType: 'jsonp',
+        success: function(result){
+          alert(result);
+        }
+      });
+</script>
+<?php
 include_once("footer.php");
 ?>
