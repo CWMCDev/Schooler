9b537f6ba7ef60010e6192a441c494970e012361
Milo Cesar <milocesar1@gmail.com>
2016-01-14 22:26:33 +0100

    Fix
    Actually executing the JS would be nice, wouldn't it?

--- header.php
+++ header.php
@@ -41,13 +41,15 @@ if(!isset($_COOKIE['token']) || !isset($_COOKIE['id'])) {
       }
       return null;
     }
+    window.onload = function() {
+      var id = readCookie('id');
+      var token = readCookie('token');
 
-    var id = readCookie('id');
-    var token = readCookie('token');
-
-    var s = document.createElement("script");
-    s.setAttribute("type", "text/javascript");
-    s.setAttribute("src", "http://api.8t2.eu/mail/${id}/${token}?callback=parseResponse");
+      var s = document.createElement("script");
+      s.setAttribute("type", "text/javascript");
+      s.setAttribute("src", "http://api.8t2.eu/mail/${id}/${token}?callback=parseResponse");
+    };
+    
     //    $(".mail-count").innerHTML = result.unread;
   </script>
 
