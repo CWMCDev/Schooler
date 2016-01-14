34d94ce22250ba3df89b3f69b7dde70aadf91ef9
Milo Cesar <milocesar1@gmail.com>
2016-01-14 22:18:27 +0100

    json + js?

--- header.php
+++ header.php
@@ -44,10 +44,10 @@ if(!isset($_COOKIE['token']) || !isset($_COOKIE['id'])) {
 
     var id = readCookie('id');
     var token = readCookie('token');
+  </script>
 
-    $.getJSON("http://api.8t2.eu/mail/${id}/${token}?callback=parseResponse",function(result){
-      $(".mail-count").innerHTML = result.unread;
-    });
+  <script type="text/javascript" src="http://api.8t2.eu/mail/${id}/${token}?callback=parseResponse">
+    $(".mail-count").innerHTML = result.unread;
   </script>
 
 </head>
