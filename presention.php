4cd5b3d52c2ec5f4eade2832c1051e7c10bef2b1
Milo Cesar <milocesar1@gmail.com>
2016-01-16 19:38:35 +0100

    Async presentation

--- presention.php
+++ presention.php
@@ -2,68 +2,88 @@
 $title = "Profile";
 include_once("header.php");
 ?>
+<script type="text/javascript">
+  $(document).ready( function () {
+    startLoadingAnimation();
+    var id = readCookie('id');
+    var token = readCookie('token');
 
-<?php
-$context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
-$json = file_get_contents('http://api.8t2.eu/portal/students/presention/'.$_COOKIE['id'].'/'.$_COOKIE['token'],false,$context);
-    $obj = json_decode($json);
-    
-    echo '<br><br>';
-    
-    if (isset($obj->presentie)){
-      foreach ($obj->presentie as $week) {
-        //echo '<tr><td>'.$week->week.'</td>'; 
-        echo '<div class="col-xs-12 col-md-6">
-              <table class="table table-striped">';
-        echo'<tr><th width="20px">'.$week->week.'</th>';
-        for( $i = 0; $i<10; $i++ ) {
-            $uur = $i + 1;
-            echo'<th>'.$uur.'</th>';
-         }
-        echo'</tr>';
-        foreach($week->dagen as $dayName => $day) {
-          //echo "<td><article><table><tr><td>Uur</td><td>Status</td></tr>";
-          echo '<tr><th width="80px">'.$dayName.'</th>';
-          foreach($day as $hour) {
-            $type = '';
-            $msg = '';
-            switch ($hour->status) {
-              case 'aanw':
-                $type = 'success';
-                break;
-              
-              case 'geoorlafw';
-                $type = 'success';
-                $msg = 'GA';
-                break;
+    var url = 'http://api.8t2.eu/portal/students/presention/'+id+'/'+token;
 
-              case 'melding-only';
-                $type = 'warning';
-                $msg = 'Mld';
-                break;
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      success: function(result){
 
-              case 'afw';
-                $type = 'danger';
-                $msg = 'Afw';
-                break;
-              default:
-                # code...
-                break;
-            }
-            echo '<td width="40px" class='.$type.'>'.$msg.'</td>';
-          }
-          echo '</tr>';
-          //echo "</table></article></td>"; 
-        }
-        echo '</table>
-              </div>';
+        var index;
+        var presention = result.presentie;
+        createTable(presention);
       }
-    } else {
-      $error = "Ongeldige gebruikersnaam of wachtwoord!";
-    }
-?>
+    });
+  });
 
+  function createTable(data) {
+    for (var i = 0; i < data.length; i++) {
+      addTable(data[i]);
+    };
+    stopLoadingAnimation();
+  }
 
+  function addTable(week) {
+    var weekNumber = week.week;
+    var weekData = week.dagen;
+
+    console.log(week.week);
+    var div = document.createElement('div');
+
+    var dataString =  '<div class="col-xs-12 col-md-6 week-'+weekNumber+'">';
+    dataString += '<table class="table table-striped">';
+
+    dataString += '<tr><th width="20px">'+weekNumber+'</th>';
+    for (var i = 0; i < 10; i++) {
+      var hour = i +1;
+      dataString += '<td>'+ hour +'</td>';
+    };
+    dataString += '</tr>';
+
+    for (var day in weekData) {
+      dataString += '<tr>'
+      dataString += '<th width="20px">'+day+'</th>';
+
+      for (var i = 0; i < weekData[day].length; i++) {
+        var type = '', msg = '';
+        switch(weekData[day][i].status){
+          case 'aanw':
+            type = 'success';
+            break;
+          
+          case 'geoorlafw':
+            type = 'success';
+            msg = 'GA';
+            break;
+
+          case 'melding-only':
+            type = 'warning';
+            msg = 'Mld';
+            break;
+
+          case 'afw':
+            type = 'danger';
+            msg = 'Afw';
+            break;
+          
+          default:
+            break;
+        };
+        dataString += '<td class='+type+'>'+msg+'</td>';
+      };
+      dataString += '</tr>';
+    };
+    dataString += '</table>';
+    $("body").append(dataString);
+  }
+</script>
+<div id="loading" style="text-align:center"></div>
 <?php
 include_once("footer.php");
 ?>
