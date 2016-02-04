68d52cda72796491f99f922b7079e3d0c4e79b24
Milo Cesar <milocesar1@gmail.com>
2016-02-04 23:41:58 +0100

    Vakanties

--- dashboard.php
+++ dashboard.php
@@ -22,12 +22,54 @@ if (isset($obj->student->name)){
 	$error = "Ongeldige gebruikersnaam of wachtwoord!";
 }
 ?>
+<div class="container">
+	<div class="col-md-6 col-sm-12">
+		<div class="panel panel-primary">
+			<div class="panel-heading">Vakanties</div>
+			<table class="table table-striped" id="vacation">
+			</table>
+		</div>
+	</div>
+</div>
 
 <script type="text/javascript">
 	function updateUnread(unread){
 		$('#mailAlert').html('Je hebt ' + unread + ' ongelezen email(s).');
 		$('#mailAlert').show();
 		console.log(unread);
+	}
+
+	Date.prototype.getWeekNumber = function(){
+      var d = new Date(+this);
+      d.setHours(0,0,0);
+      d.setDate(d.getDate()+4-(d.getDay()||7));
+      return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+    };
+
+	$(document).ready( function () {
+		var url = 'http://api.8t2.eu/vacations';
+
+		$.ajax({
+	      url: url,
+	      dataType: 'jsonp',
+	      success: function(result){
+	        showVacation(result);
+	      }
+	    });
+	});
+
+	function showVacation(data){
+		var dataString = '';
+		for(i = 0; i < data.vacations.length; i++){
+			var vacation = data.vacations[i];
+			var endDate = new Date(vacation.end.replace( /(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+			var now = new Date();
+
+			if(endDate > now){
+				dataString += '<tr><td>' + vacation.name + '</td><td>' + vacation.start + '</td><td>' + vacation.end + '</td></tr>';
+			}
+		}
+		$("#vacation").html(dataString);
 	}
 </script>
 
