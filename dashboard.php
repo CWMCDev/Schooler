25682346a0e4797d6aaff4d0c5adbcf053c5c3a9
Milo Cesar <milocesar1@gmail.com>
2016-02-04 22:29:59 +0100

    Removed Dtdl

--- dashboard.php
+++ dashboard.php
@@ -5,12 +5,17 @@ include_once("header.php");
 <?php
 $context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
 $json = file_get_contents('http://api.8t2.eu/portal/students/profile/'.$_COOKIE['id'].'/'.$_COOKIE['token'],false,$context);
-    $obj = json_decode($json);
-    if (isset($obj->student->name)){
-      echo $obj->student->name;
-    } else {
-      $error = "Ongeldige gebruikersnaam of wachtwoord!";
-    }
+$obj = json_decode($json);
+if (isset($obj->student->name)){
+	?>
+	<div class="page-header">
+		<h1>Hello, <?php echo($obj->student->name); ?>!</h1>
+	</div>
+</div>
+<?php
+} else {
+	$error = "Ongeldige gebruikersnaam of wachtwoord!";
+}
 ?>
 
 <?php
