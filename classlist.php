467778d5b17caeec869df2915cc5c2b9b157281b
Stein Carsten Sinke <sinke.carsten95@gmail.com>
2016-02-04 21:00:05 +0100

    first update classlist, still not working

--- classlist.php
+++ classlist.php
@@ -3,6 +3,44 @@ $title = "Profile";
 include_once("header.php");
 ?>
 
+<script type="text/javascript">
+  $(document).ready( function ()  {
+    startLoadingAnimation();
+    var id = readCookie('id');
+    var token = readCookie('token');
+
+    var url = 'http://api.8t2.eu/portal/students/classlist/'+id+'/'+token;
+
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      success: function(result){
+
+        var index;
+        var classlist = result.classList;
+        createTable(presention);
+      }
+    });
+  });
+
+  function createTable(data)  {
+    for (var i = 0; i < data.length; i++) {
+      addTable(data[i]);
+    };
+    stopLoadingAnimation();
+  }
+
+  function showClasslist(data) {
+    for (var person in data.classlist) {
+      datastring += '<tr>'
+      datastring += '<th width="40px"></th>';
+    }
+  }
+</script>
+
+
+
+
 <?php
 $context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
 $json = file_get_contents('http://api.8t2.eu/portal/students/classlist/'.$_COOKIE['id'].'/'.$_COOKIE['token'],false,$context);
