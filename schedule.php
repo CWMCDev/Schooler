569e2022695fde023a9a71a7e797c3fae71fcb15
chrisje <chriswitteveen2@hotmail.com>
2016-02-05 02:02:05 +0100

    testing fullcalendar

--- schedule.php
+++ schedule.php
@@ -69,19 +69,72 @@ if(!$isCodeSet){
 
   function showSchedule(data){
     $('#calendar').fullCalendar({
-    defaultView: 'agendaDay',
-    events: [
-        // events go here
-    ],
-    resources: [
-        { id: 'a', title: 'Maandag' },
-        { id: 'b', title: 'Dinsdag' },
-        { id: 'c', title: 'Woensdag' },
-        { id: 'd', title: 'Donderdag' },
-        { id: 'e', title: 'Vrijdag'}
-    ]
-  });
-    
+     header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'agendaWeek,agendaDay'
+			},
+			defaultDate: '2016-01-12',
+			editable: true,
+			eventLimit: true, // allow "more" link when too many events
+			events: [
+				{
+					title: 'All Day Event',
+					start: '2016-01-01'
+				},
+				{
+					title: 'Long Event',
+					start: '2016-01-07',
+					end: '2016-01-10'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2016-01-09T16:00:00'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2016-01-16T16:00:00'
+				},
+				{
+					title: 'Conference',
+					start: '2016-01-11',
+					end: '2016-01-13'
+				},
+				{
+					title: 'Meeting',
+					start: '2016-01-12T10:30:00',
+					end: '2016-01-12T12:30:00'
+				},
+				{
+					title: 'Lunch',
+					start: '2016-01-12T12:00:00'
+				},
+				{
+					title: 'Meeting',
+					start: '2016-01-12T14:30:00'
+				},
+				{
+					title: 'Happy Hour',
+					start: '2016-01-12T17:30:00'
+				},
+				{
+					title: 'Dinner',
+					start: '2016-01-12T20:00:00'
+				},
+				{
+					title: 'Birthday Party',
+					start: '2016-01-13T07:00:00'
+				},
+				{
+					title: 'Click for Google',
+					url: 'http://google.com/',
+					start: '2016-01-28'
+				}
+			]
+    });
+  
     console.log(data);1
   }
 </script>
