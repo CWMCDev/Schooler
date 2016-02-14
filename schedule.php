f84cda8d99da7632503b7d17c47d412b6658a825
chrisje <chriswitteveen2@hotmail.com>
2016-02-14 20:05:22 +0100

    fixed timezone

--- schedule.php
+++ schedule.php
@@ -157,7 +157,7 @@ if(!$isCodeSet){
               $backgroundColor = 'FF0000';
             }
             
-            echo '{ id: \''.$lesson->id.'\', start: \''.date("Y-m-d\TH:i:s",$lesson->start).'\', end: \''.date("Y-m-d\TH:i:s",$lesson->end).'\', title: \''.$subject.'\n'.$teacher.'\n'.$location.'\', backgroundColor: \''.$backgroundColor.'\', textColor: \'black\'},';
+            echo '{ id: \''.$lesson->id.'\', start: \''.date("Y-m-d\TH:i:s",$lesson->start."+01:00").'\', end: \''.date("Y-m-d\TH:i:s",$lesson->end."+01:00").'\', title: \''.$subject.'\n'.$teacher.'\n'.$location.'\', backgroundColor: \''.$backgroundColor.'\', textColor: \'black\'},';
           }
         ?>
 			],
