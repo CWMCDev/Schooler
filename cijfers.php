73201c92f726eef6d192a23b263d38b2265a1ed6
Stein Carsten Sinke <sinke.carsten95@gmail.com>
2016-02-14 21:15:43 +0100

    moving the script again

--- cijfers.php
+++ cijfers.php
@@ -11,13 +11,13 @@ include_once("header.php");
 </form>
 <div id="loading" style="text-align:center"></div>
 
+<div id="grades"></div>
+
 <script>
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover(); 
 });
 </script>
-
-<div id="grades"></div>
 
 <!--loading and output grades-->
 <script type="text/javascript">
