cbcc64dcb57ba34d973227f91530fd5c29c74d5c
chrisje <chriswitteveen2@hotmail.com>
2016-02-14 22:27:55 +0100

    lets try again

--- cijfers.php
+++ cijfers.php
@@ -64,7 +64,7 @@ $(document).ready(function(){
       for (var j = 0; j < maxCount; j++) {
         var grade = cls.grades[j];
         if(typeof grade != 'undefined'){
-          dataString += '<td width="25px"><button type="button" class="btn btn-default" data-toggle="popover" data-placement="top" data-content="'+grade.Beschrijving+'\nWeging:'+grade.Weging+'">' + grade.Cijfer + '</button></td>';
+          dataString += '<td width="25px"><button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="top" data-content="'+grade.Beschrijving+'\nWeging:'+grade.Weging+'">' + grade.Cijfer + '</button></td>';
         }else{
           dataString += '<td width="25px"> </td>';
         }
