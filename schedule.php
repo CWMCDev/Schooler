8bb0b10d30bdbe619f79c0720b0720dd62b50685
chrisje <chriswitteveen2@hotmail.com>
2016-02-14 21:37:20 +0100

    Maybe carsten was right

--- schedule.php
+++ schedule.php
@@ -41,7 +41,6 @@ if(!$isCodeSet){
     schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
 			defaultView: 'agendaWeek',
 			defaultDate: moment(),
-      timezone: 'Europe/Amsterdam',
       height: 800,
       slotDuration: '00:15:00',
       minTime: '08:00:00',
@@ -123,7 +122,7 @@ if(!$isCodeSet){
               $backgroundColor = 'FF0000';
             }
             
-            echo '{ id: \''.$lesson->id.'\', start: \''.date("Y-m-d\TH:i:s",$lesson->start).'\', end: \''.date("Y-m-d\TH:i:s",$lesson->end).'\', title: \''.$subject.'\n'.$teacher.'\n'.$location.'\', backgroundColor: \''.$backgroundColor.'\', textColor: \'black\'},';
+            echo '{ id: \''.$lesson->id.'\', start: \''.date("Y-m-d\TH:i:s",$lesson->start).'+06:00'.'\', end: \''.date("Y-m-d\TH:i:s",$lesson->end).'+06:00.'\', title: \''.$subject.'\n'.$teacher.'\n'.$location.'\', backgroundColor: \''.$backgroundColor.'\', textColor: \'black\'},';
           }
         ?>
 			],
