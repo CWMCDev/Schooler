a5afd532d3d060adfb76f77502d679089dc4da1e
cwitteveen <chriswitteveen2@hotmail.com>
2016-03-23 09:07:10 +0100

    https -> http

--- dashboard.php
+++ dashboard.php
@@ -9,7 +9,7 @@ include_once("header.php");
 <!--display student name-->
 <?php
 $context = stream_context_create(array('http' => array('header'=>'Connection: close\r\n')));
-$json = file_get_contents('https://api.8t2.eu/portal/students/profile/'.$_COOKIE['id'].'/'.$_COOKIE['token'],false,$context);
+$json = file_get_contents('http://api.8t2.eu/portal/students/profile/'.$_COOKIE['id'].'/'.$_COOKIE['token'],false,$context);
 $obj = json_decode($json);
 if (isset($obj->student->name)){
 	?>
@@ -48,7 +48,7 @@ if (isset($obj->student->name)){
     };
 
 	$(document).ready( function () {
-		var url = 'https://api.8t2.eu/vacations';
+		var url = 'http://api.8t2.eu/vacations';
 
 		$.ajax({
 	      url: url,
